4de093fe5937a3ec6c3333ba9b1896a189fba499
Douglas Scheurich <dougscheurich@gmail.com>
2025-06-19 03:09:50 -0400

    Changing to add tuple print function

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
-#include <iostream>
-
-void greet() {
-    std::cout << "Greetings Universe!!!" << std::endl;
-}
+#include "print_tuple.h"
+#include <string>
+#include <tuple>
 
 int main() {
-    greet();
+    auto data = std::make_tuple(17, 3.14159, 'B', std::string("Universe"), std::make_pair(1, 2));
+    
+    std::cout << "Tuple contents: ";
+    utils::tuple::print_tuple(std::cout, data); 
+    std::cout << "\n" << std::endl;
+    
     return 0;
 }
